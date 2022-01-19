942632f3a6fee37a586937018dd0e4c79a067720
leandrobonacompass <leandro.silva_bols@compasso.com.br>
2022-01-19 09:10:41 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,55 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const GNRequest = require('./apis/gerencianet');
+  
+  const app = express();
+  
+  app.use(bodyParser.json());
+  
+  app.set('view engine', 'ejs');
+  app.set('views', 'src/views');
+  
+  const reqGNAlready = GNRequest({
+    clientID: process.env.GN_CLIENT_ID,
+    clientSecret: process.env.GN_CLIENT_SECRET
+  });
+  
+  app.get('/', async (req, res) => {
+    const reqGN = await reqGNAlready;
+    const dataCob = {
+      calendario: {
+        expiracao: 3600
+      },
+      valor: {
+        original: '0.10'
+      },
+      chave: 'ab79232a-840c-4f95-b98b-9e1903c3219d',
+      solicitacaoPagador: 'Cobrança dos serviços prestados.'
+    };
+  
+    const cobResponse = await reqGN.post('/v2/cob', dataCob);
+    const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`);
+  
+    res.render('qrcode', { qrcodeImage: qrcodeResponse.data.imagemQrcode })
+  });
+  
+  app.get('/cobrancas', async(req, res) => {
+    const reqGN = await reqGNAlready;
+  
+    const cobResponse = await reqGN.get('/v2/cob?inicio=2021-02-15T16:01:35Z&fim=2021-02-22T23:59:00Z');
+  
+    res.send(cobResponse.data);
+  });
+  
+  app.post('/webhook(/pix)?', (req, res) => {
+    console.log(req.body);
+    res.send('200');
+  });
+  
+  app.listen(8000, () => {
+    console.log('running');
+  })
